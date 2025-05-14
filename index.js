30e487d2e99011f03d5b760e9644e6c086c4e58c
Enrique Lazaro <enriquee0213@gmail.com>
2025-05-14 00:21:47 -0400

    feat: Initial Commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const supabaseClient = require('@supabase/supabase-js');
+const bodyParser = require('body-parser');
+const { isValidStateAbbreviation } = require('usa-state-validator');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+const supabaseURL = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = supabaseClient.createClient(supabaseURL, supabaseKey);
+
+
+app.get('/customers', async (req, res) => {
+    console.log('attempting to get customers');
+
+    const { data, error } = await supabase.from('customer').select()
+
+    if (error) {
+        console.log(`Error: ${error}`);
+        res.statusCode = 500;
+        res.send(error);
+    }
+
+    res.send(data);
+});
+
+app.post('/customer', async (req,res) => {
+    console.log('adding customer');
+
+    console.log(req.body);
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const state = req.body.state;
+
+    if(!isValidStateAbbreviation(state)) {
+        console.error(`State: ${state} is invalid`);
+        res.statusCode = 400;
+        res.header('Content-Type', 'application/json');
+        const errorJson = {
+            'message': `State: ${state} is not valid`
+        }
+
+        res.send(JSON.stringify(errorJson));
+        return;
+    }
+
+    const { data, error } = await supabase
+    .from('customer')
+    .insert({ 
+        customer_first_name:firstName, 
+        customer_last_name:lastName, 
+        customer_state:state 
+    })
+    .select()
+
+    if (error) {
+        console.log(`Error: ${error}`);
+        res.statusCode = 500;
+        res.send(error);
+    }
+
+    res.send(data);
+})
+
+app.listen(port, () => {
+    console.log('APP IS  more than ALIVEEEEE on port', port);
+});
