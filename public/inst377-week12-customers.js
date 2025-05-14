a00d160423743afc3fdd56e7901e0234fdf372c5
Enrique Lazaro <enriquee0213@gmail.com>
2025-05-14 01:23:59 -0400

    feat: Updating files

---
+++ public/inst377-week12-customers.js
@@ -0,0 +1,44 @@
+async function loadCustomerData() {
+    await fetch('/customers')
+        .then((result) => result.json())
+        .then((resultJson) => {})
+        const table = document.createElement('table');
+        table.setAttribute('id', 'customerInfo');
+
+        const tableRow = document.createElement('tr');
+
+        const tableHeadingFirstName = document.createElement('th');
+        tableHeadingFirstName.innerHTML = 'First Name';
+        tableRow.appendChild(tableHeadingFirstName);
+
+        const tableHeadingLastName = document.createElement('th');
+        tableHeadingLastName.innerHTML = 'Last Name';
+        tableRow.appendChild(tableHeadingLastName);
+
+        const tableHeadingState = document.createElement('th');
+        tableHeadingState.innerHTML = 'State';
+        tableRow.appendChild(tableHeadingState);
+
+        table.appendChild(tableRow);
+
+        resultJson.forEach((customer) => {
+            const customerTableRow = document.createElement('tr');
+            const customerTableFirstName = document.createElement('td');
+            const customerTableLastName = document.createElement('td');
+            const customerTableState = document.createElement('td');
+
+            customerTableFirstName.innerHTML = customer.customer_first_name;
+            customerTableLastName.innerHTML = customer.customer_last_name;
+            customerTableState.innerHTML = customer.customer_state;
+
+            customerTableRow.appendChild(customerTableFirstName);
+            customerTableRow.appendChild(customerTableLastName);
+            customerTableRow.appendChild(customerTableState);
+
+            table.appendChild(customerTableRow);
+        });
+
+        document.body.appendChild(table);
+}
+
+window.onload = loadCustomerData;
